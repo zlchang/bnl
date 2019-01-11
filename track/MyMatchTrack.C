fd6dfe7a1cd1e76df66d7bbfde69349e2ef340c0
Zilong Chang <achangzl@hotmail.com>
2019-01-11 13:25:05 -0500

    updates

--- track/MyMatchTrack.C
+++ track/MyMatchTrack.C
@@ -1,7 +1,8 @@
 void MyMatchTrack(
-		  const char* MuDst = "test.list",//~/data_embed/pp500_production_2012/pt11_15_100_20160201/P13ib.SL13b/2012/109/13109027/pt11_15_st_zerobias_adc_13109027_raw_1600001_7.MuDst.root",///global/homes/z/zchang/data_embed/13109027/pt11_15_st_zerobias_adc_13109027_raw_4600001_8.MuDst.root",///star/u/zchang/2014-04-embedding/embedding/test.MuDst.root",
+		  const char* MuDst = "test.emb.list",//~/data_embed/pp500_production_2012/pt11_15_100_20160201/P13ib.SL13b/2012/109/13109027/pt11_15_st_zerobias_adc_13109027_raw_1600001_7.MuDst.root",///global/homes/z/zchang/data_embed/13109027/pt11_15_st_zerobias_adc_13109027_raw_4600001_8.MuDst.root",///star/u/zchang/2014-04-embedding/embedding/test.MuDst.root",
 		  //const char* MuDst = "/star/u/zchang/public/MuDst/st_physics_13109014_raw_*.MuDst.root",
-		  const char* Outfile = "MyMatchTrackToEmc.root"
+		  const char* Outfile = "MyMatchTrackToEmc.root",
+		  bool flag = true
 		  )
 {
   gROOT->Macro("loadMuDst.C");
@@ -26,10 +27,14 @@ void MyMatchTrack(
   //  StMuDst* muDst = muDstMaker->muDst();
 
   StEEmcDbMaker* eemcDb = new StEEmcDbMaker;
-  
+ 
+  //StTriggerFilterMaker *trgfilt = new StTriggerFilterMaker;
+  //trgfilt->addTrigger(380403);
+ 
   StTriggerSimuMaker* simuTrig = new StTriggerSimuMaker;
   simuTrig->useOfflineDB(); 
-  simuTrig->setMC(0); // 0=data, 1=simulation, 2=embedding
+  if(!flag) simuTrig->setMC(0); // 0=data, 1=simulation, 2=embedding
+  else simuTrig->setMC(2);
   simuTrig->useBemc();
   simuTrig->useEemc();
   simuTrig->bemc->setConfig(StBemcTriggerSimu::kOnline);
@@ -37,6 +42,7 @@ void MyMatchTrack(
   StMyTrackProjEmcPtRadius *proj = new StMyTrackProjEmcPtRadius;
   //StMyTrackProjEmcSameTower *proj = new StMyTrackProjEmcSameTower;
   StMyMatchTrackToEmcMaker *matchMaker = new StMyMatchTrackToEmcMaker;
+  matchMaker->setMc(flag);
   matchMaker->addVertexCut(new StMyVertexZCut(30.));
   matchMaker->setTrackProjEmc(proj);
   matchMaker->SetOutfile(Outfile);
